6bd1626faf135bf842adf4e2ae4d9a0ca1bdfb17
Maisha Thasin <mthasin@wealthsimple.com>
2022-07-11 22:15:12 -0400

    css changes

--- app/app.py
+++ app/app.py
@@ -81,7 +81,7 @@ def login():
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
-                return redirect(url_for('dashboard'))
+                return redirect(url_for('data_summary.html'))
     return render_template('login.html', form=form)
 
 
