8decdb934e40f825e6cb3f07693556186eb783ab
Shreya Ponugoti <sp843@cornell.edu>
2022-07-18 23:31:47 -0500

    Fixed date bugs

--- app/app.py
+++ app/app.py
@@ -28,11 +28,6 @@ def data_summary_page():
 
 def get_prices(list):
     api_codes = {
-        # "Corn": "CHRIS/CME_C1",
-        # "Oats": "CHRIS/CME_O1",
-        # "Wheat": "CHRIS/CME_W1",
-        # "Soybean": "CHRIS/CME_S1",
-        # "Dairy": "CHRIS/CME_DA1",
         "Arabica Coffee": "COFFEE",
         "Brent Crude Oil": "BRENTOIL",
         "Cocoa": "COCOA",
@@ -40,25 +35,24 @@ def get_prices(list):
         "Natural Gas": "NG",
     }
     prices_dict = {}
-
+    today = datetime.datetime.now().date()
+    date = get_date(today)
     for item in list:
-        url = "https://commodities-api.com/api/timeseries?access_key=cqmirudsx2mdaprj2o1s2ftv838j8n3ra5q8cge1s195uxsk8sv3jw4bddfo&base=USD" + \
+        url = "https://commodities-api.com/api/timeseries?access_key=cqmirudsx2mdaprj2o1s2ftv838j8n3ra5q8cge1s195uxsk8sv3jw4bddfo&start_date=" + str(date) + "&end_date=" + str(today) + "&symbols=" + \
             api_codes[item]
-        # url = "https://data.nasdaq.com/api/v3/datasets/" + \
-        #     api_codes[item] + "?api_key=7PvJ27fqD-s1fgvKWZgk"
+        print(url)
         response = requests.get(url)
         jsonResponse = response.json()
-        today = datetime.datetime.now().date()
-        date = get_date(today)
         print(date)
-        price_today = 1/jsonResponse["data"]["rates"][today][api_codes[item]]
-        price_at_date = 1/jsonResponse["data"]["rates"][date][api_codes[item]]
+        print(jsonResponse["data"])
+        price_today = 1 / \
+            jsonResponse["data"]["rates"][str(today)][api_codes[item]]
+        print(price_today)
+        price_at_date = 1 / \
+            jsonResponse["data"]["rates"][str(date)][api_codes[item]]
         prices_dict[item] = [price_today,
                              get_change(price_today, price_at_date)]
         print(prices_dict[item])
-        # prices_dict[item] = [jsonResponse["dataset"]["data"]
-        #                      [0][1], jsonResponse["dataset"]["data"][0][5]]
-        # use dict["dataset"]["data"][0][5] for change in price - change not present for all items though
     return prices_dict
 
 
