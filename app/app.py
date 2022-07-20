a6a8633bd3f872f3850935dbed714bad55ef686a
Natalie Volk <natalievolk@rogers.com>
2022-07-20 16:20:43 -0400

    Parse data and add to graph

--- app/app.py
+++ app/app.py
@@ -46,25 +46,27 @@ def detailed_data_page():
     print(watchlist)
 
     # to be added back in after API is implemented
-    prices_dict = get_time_series_prices(selected_item)
-    # print(prices_dict)
-    # data = prices_dict[selected_item]
-    # print(data)
+    prices_dict, api_code = get_time_series_prices(selected_item)
+    labels, data = parse_data_for_graph(prices_dict, api_code)
 
+    print(labels)
+    print(data)
+
+    return render_template("data_detailed.html", item=selected_item, labels=labels, values=data)
 
     # Keep for now just in case
-    dummy_data = [
-        (1, 5),
-        (2, 7),
-        (3, 2),
-        (4, 4),
-        (5, 10)
-    ]
+    # dummy_data = [
+    #     (1, 5),
+    #     (2, 7),
+    #     (3, 2),
+    #     (4, 4),
+    #     (5, 10)
+    # ]
 
-    dummy_labels = [row[0] for row in dummy_data]
-    dummy_values = [row[1] for row in dummy_data]
+    # dummy_labels = [row[0] for row in dummy_data]
+    # dummy_values = [row[1] for row in dummy_data]
 
-    return render_template("data_detailed.html", item=selected_item, labels=dummy_labels, values=dummy_values)
+    # return render_template("data_detailed.html", item=selected_item, labels=dummy_labels, values=dummy_values)
 
 
 def make_call(url):
@@ -131,19 +133,26 @@ def get_time_series_prices(item):
     prices_dict = {}
     today = datetime.today()
     yesterday = (today - timedelta(days=1)).date()
-    print(yesterday)
     lastMonthDate = get_date(today, 30)
-    print(lastMonthDate)
 
     url = "https://commodities-api.com/api/timeseries?access_key=0yw3m4s5g7fz8ialo49gqxtjnxbto11t59jg5qw4krdo15j48v37q06hgsnx&start_date=" + str(lastMonthDate) + "&end_date=" + str(yesterday) + "&symbols=" + \
         api_codes[item]
     response = requests.get(url)
     jsonResponse = response.json()
-    print(jsonResponse)
     prices_dict = jsonResponse["data"]["rates"]
-    print(prices_dict)
 
-    return prices_dict
+    return prices_dict, api_codes[item]
+
+
+def parse_data_for_graph(prices_dict, api_code):
+    labels = []
+    data = []
+
+    for date in prices_dict:
+        labels.append(date)
+        data.append(1 / prices_dict[date][api_code])
+
+    return labels, data
 
 
 def get_date(today, numberOfDays):
